c845858bc924b2e2b49aa82ecd85564203201e9a
Pavel Karyapkin <karyapkin@gmail.com>
2022-12-22 19:03:50 +0600

    switch to TS CartBtn

---
+++ src/components/Navigation/CartBtn/CartBtn.tsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import classes from './CartBtn.module.scss';
+
+const CartBtn = (props: {
+    count: number,
+    price: number,
+}) => {
+	const {	count, price } = props;
+
+	return(
+		<NavLink
+			to={'/order'}
+			activeClassName={classes.active}
+			className={classes.cartBtn}>
+			<i className={[classes.icon, 'material-icons'].join(' ')}>shopping_cart</i>
+			<div className={classes.info}>
+				<p className={classes.price}>
+					<span>{price}</span>
+					<span> ₸</span>
+				</p>
+				<p className={classes.amount}>
+					<span>{count}</span>
+					<span> товаров</span>
+				</p>
+			</div>
+		</NavLink>
+	);
+}
+
+export default CartBtn;
